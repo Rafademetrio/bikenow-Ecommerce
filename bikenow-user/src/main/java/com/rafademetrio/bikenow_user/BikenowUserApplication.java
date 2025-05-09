1a30ff1cfd34c3e4dbaa5f07c02d6b3f653ad400
Rafael Lucena <rafademetrio@gmail.com>
2025-05-09 10:35:51 -0300

    user-branch and user-service created

--- bikenow-user/src/main/java/com/rafademetrio/bikenow_user/BikenowUserApplication.java
+++ bikenow-user/src/main/java/com/rafademetrio/bikenow_user/BikenowUserApplication.java
@@ -1,13 +1,13 @@
-package com.rafademetrio.bikenow_gateway;
+package com.rafademetrio.bikenow_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BikenowGatewayApplication {
+public class BikenowUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BikenowGatewayApplication.class, args);
+		SpringApplication.run(BikenowUserApplication.class, args);
 	}
 
 }
--- bikenow-user/src/test/java/com/rafademetrio/bikenow_user/BikenowUserApplicationTests.java
+++ bikenow-user/src/test/java/com/rafademetrio/bikenow_user/BikenowUserApplicationTests.java
@@ -1,10 +1,10 @@
-package com.rafademetrio.bikenow_gateway;
+package com.rafademetrio.bikenow_user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BikenowGatewayApplicationTests {
+class BikenowUserApplicationTests {
 
 	@Test
 	void contextLoads() {
